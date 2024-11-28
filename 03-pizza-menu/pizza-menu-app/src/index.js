4c25e7e937b1cdf8b362537db3b50ad950d355c0
Helio Dourado <heliohdd@yahoo.com.br>
2024-11-28 09:10:30 -0300

    Section 5.47 - Conditional Rendering With &&

--- 03-pizza-menu/pizza-menu-app/src/index.js
+++ 03-pizza-menu/pizza-menu-app/src/index.js
@@ -69,14 +69,21 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
+  console.log("Num Pizzas: ", numPizzas);
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
       {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
@@ -108,16 +115,23 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 8;
+  // const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
+  console.log("Is it open? ", isOpen);
 
   // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
   // else alert("Sorry, we're closed!");
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're currently open!
+      <div className="order">
+        {isOpen && (
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+        )}
+        <button className="btn">Order</button>
+      </div>
     </footer>
   );
   // return React.createElement("footer", null, "We're currently open!");

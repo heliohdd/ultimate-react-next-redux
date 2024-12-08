fa56d0a2e8658d1d9ff3683e782fcfbae98cf1e0
Helio Dourado <heliohdd@yahoo.com.br>
2024-12-08 10:55:57 -0300

    Section 12.156 - Cleaning Up Data Fetching

--- 07-use-popcorn/usepopcorn-app/src/App.jsx
+++ 07-use-popcorn/usepopcorn-app/src/App.jsx
@@ -103,12 +103,14 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok) {
@@ -120,10 +122,13 @@ export default function App() {
           if (data.Response === "False") throw new Error("Movie not found");
 
           setMovies(data.Search);
-          console.log(data.Search);
+          setError("");
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -136,6 +141,9 @@ export default function App() {
       }
 
       fetchMovies();
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
